5805d62062a26b334c12fadb95ec100f9e5c9417
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-04-15 21:58:24 +0300

    make build

---
+++ landing_page_final_review.410384f5.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){console.log("Project is ready!"),document.querySelectorAll(".nav__link").forEach(function(e){e.addEventListener("click",function(t){t.preventDefault();var n=e.getAttribute("href"),o=document.querySelector(n);o&&o.scrollIntoView({behavior:"smooth"})})})});
+//# sourceMappingURL=landing_page_final_review.410384f5.js.map
